568181514540b8b00e74fc53b9392e00c9c8a068
saybb <30707324+saybb@users.noreply.github.com>
2018-10-17 20:41:45 +1100

    Revert "Merge branch 'mapping' into escape"
    This reverts commit ad3fe3f1f11ae535dc971b4c5b28d9adc699584c, reversing
    changes made to a0082b6b4d78c0f5066d3b962273019eb85feeef.

--- src/mycontroller/map/Mapping.java
+++ src/mycontroller/map/Mapping.java
@@ -20,64 +20,31 @@ import utilities.Coordinate;
 
 public class Mapping {
 	private HashMap<Coordinate, Integer> keys;
-<<<<<<< HEAD
 	private HashMap<Coordinate, String> pointsOfInterest;
 	public static Mapping map;
-=======
-	private HashMap<Coordinate, MapTile> pointsOfInterest;
-	private ArrayList<Coordinate> deadEnds;
->>>>>>> mapping
 	
 	public Mapping() {
 		keys = new HashMap<>();
 		pointsOfInterest = new HashMap<>();
 	}
 	
-	// Returns keys that have been seen by player
 	public HashMap<Coordinate, Integer> getKeysSeen() {
 		return keys;
 	}
 	
-	public HashMap<Coordinate, MapTile> getPointsOfInterest() {
+	public HashMap<Coordinate, String> getPointsOfInterest() {
 		return pointsOfInterest;
 	}
 	
-<<<<<<< HEAD
 	public void addFoundKey(Coordinate coordinate, Integer value) {
 		keys.put(coordinate, value);
 	}
 	
 	public void addPointOfInterest(Coordinate coordinate, String type) {
 		pointsOfInterest.put(coordinate, type);
-=======
-	public ArrayList<Coordinate> getDeadEnds() {
-		return deadEnds;
-	}
-	
-	public MapTile getTypeByCoordinate(Coordinate coordinate) {
-		// Check if there is a trap and which type of trap it is
-		for (Map.Entry<Coordinate, MapTile> mapInfo : pointsOfInterest.entrySet()) {
-			if (mapInfo.getKey().equals(coordinate)) {
-				return mapInfo.getValue();
-			}
-		}
-		
-		// Check whether the type of mapTile at the point is a wall
-		for (Coordinate deadEnd : deadEnds) {
-			if (coordinate.equals(deadEnd)) {
-				return new MapTile(MapTile.Type.WALL);
-			}
-		}
-		return null;
-	}
-	
-	public void addPointOfInterest(Coordinate coordinate, MapTile mapTile) {
-		pointsOfInterest.put(coordinate, mapTile);
->>>>>>> mapping
 	}
 	
 	public void articulateViewPoint(HashMap<Coordinate, MapTile> currentView) {
-		
 		// Iterate through all the tiles that the car can currently see
         for (Map.Entry<Coordinate, MapTile> mapInfo : currentView.entrySet()) {
         		Coordinate coordinate = mapInfo.getKey();
@@ -93,7 +60,6 @@ public class Mapping {
 				}
 			}
         		
-<<<<<<< HEAD
         		// Found a Mud Trap i.e. game over when traversed over
         		if (mapInfo.getValue() instanceof MudTrap && !pointsOfInterest.containsKey(coordinate)) {
         			addPointOfInterest(coordinate, type);
@@ -111,35 +77,5 @@ public class Mapping {
         		System.out.println("The coordinate " + point.getKey() + " is the tile of " + point.getValue() + " !");
         }
         System.out.println("---------------All Dead Ends Processed---------------");
-=======
-        		// Check if the tile being inspected is a trap
-        		if (mapTile instanceof TrapTile && !pointsOfInterest.containsKey(coordinate)) {
-        			TrapTile trapTile = (TrapTile) mapTile;
-        			String type = trapTile.getTrap();
-        			switch (type) {
-        				case "lava":
-        					LavaTrap lavaTrap = (LavaTrap) trapTile;
-        					int key = lavaTrap.getKey();
-        					
-        					// A key exists within the lava
-        					if (key > 0 && !keys.containsValue(key)) {
-        						keys.put(mapInfo.getKey(), key);
-        					}
-        					addPointOfInterest(coordinate, mapTile);
-        					break;
-        					
-        				// One of the other traps
-        				default:
-        					addPointOfInterest(coordinate, mapTile);
-        					break;
-        			}
-        		}
-        		
-        		// In case the map tile is a dead end
-        		if (mapTile.getType().toString().equals("WALL")) {
-        			deadEnds.add(coordinate);
-        		}
-		}
->>>>>>> mapping
 	}
 }
