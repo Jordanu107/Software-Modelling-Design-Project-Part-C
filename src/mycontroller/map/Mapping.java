9aa56d80d896ea77b63b1a8506ff15509f202584
saybb <30707324+saybb@users.noreply.github.com>
2018-10-17 12:15:46 +1100

    Revert "Merge branch 'master' of github.com:Jordanu107/SWEN30006_PART_C"
    This reverts commit b215742ff70a949168270d1b8673fcd21f118941, reversing
    changes made to 7cc1780383823589d0387373ee9df7b8f22f1588.

--- src/mycontroller/map/Mapping.java
+++ src/mycontroller/map/Mapping.java
@@ -9,7 +9,6 @@ import tiles.LavaTrap;
 import tiles.MapTile;
 import tiles.MapTile.Type;
 import tiles.MudTrap;
-import tiles.TrapTile;
 import utilities.Coordinate;
 
 /**
@@ -22,12 +21,10 @@ import utilities.Coordinate;
 public class Mapping {
 	private HashMap<Coordinate, Integer> keys;
 	private HashMap<Coordinate, String> pointsOfInterest;
-	private ArrayList<Coordinate> deadEnds;
 	
 	public Mapping() {
 		keys = new HashMap<>();
 		pointsOfInterest = new HashMap<>();
-		deadEnds = new ArrayList<>();
 	}
 	
 	public HashMap<Coordinate, Integer> getKeysSeen() {
@@ -38,6 +35,10 @@ public class Mapping {
 		return pointsOfInterest;
 	}
 	
+	public void addFoundKey(Coordinate coordinate, Integer value) {
+		keys.put(coordinate, value);
+	}
+	
 	public void addPointOfInterest(Coordinate coordinate, String type) {
 		pointsOfInterest.put(coordinate, type);
 	}
@@ -46,46 +47,34 @@ public class Mapping {
 		// Iterate through all the tiles that the car can currently see
         for (Map.Entry<Coordinate, MapTile> mapInfo : currentView.entrySet()) {
         		Coordinate coordinate = mapInfo.getKey();
-        		MapTile mapTile = mapInfo.getValue();
+        		String type = mapInfo.getValue().getType().toString();
+			
+        		// Check every lava tile for any keys within the tile
+        		if (mapInfo.getValue() instanceof LavaTrap) {
+				LavaTrap lavaTrap = (LavaTrap) mapInfo.getValue();
+				
+				// A key exists within the lava
+				if (lavaTrap.getKey() > 0 && !keys.containsKey(mapInfo.getKey())) {
+					keys.put(mapInfo.getKey(), lavaTrap.getKey());
+				}
+			}
         		
-        		// Check if the tile being inspected is a trap
-        		if (mapTile instanceof TrapTile && !pointsOfInterest.containsKey(coordinate)) {
-        			TrapTile trapTile = (TrapTile) mapTile;
-        			String type = trapTile.getTrap();
-        			switch (type) {
-        				case "lava":
-        					LavaTrap lavaTrap = (LavaTrap) trapTile;
-        					int key = lavaTrap.getKey();
-        					
-        					// A key exists within the lava
-        					if (key > 0 && !keys.containsKey(key)) {
-        						keys.put(mapInfo.getKey(), key);
-        					}
-        					addPointOfInterest(coordinate, type);
-        					break;
-        				// One of the other traps
-        				default:
-        					addPointOfInterest(coordinate, type);
-        					break;
-        			}
-        		}
-        		
-        		// In case the map tile is a dead end
-        		if (mapTile.getType().toString().equals("WALL")) {
-        			deadEnds.add(coordinate);
+        		// Found a Mud Trap i.e. game over when traversed over
+        		if (mapInfo.getValue() instanceof MudTrap && !pointsOfInterest.containsKey(coordinate)) {
+        			addPointOfInterest(coordinate, type);
         		}
 		}
         
-//        // Print location and key #
-//        for (Map.Entry<Coordinate, Integer> key : keys.entrySet()) {
-//        		System.out.println("The key " + key.getValue() + " is at " + key.getKey());
-//        }
-//        System.out.println("---------------All Keys Processed---------------");
-//        
-//        // Print location of dead ends
-//        for (Map.Entry<Coordinate, String> point : pointsOfInterest.entrySet()) {
-//        		System.out.println("The coordinate " + point.getKey() + " is the tile of " + point.getValue() + " !");
-//        }
-//        System.out.println("---------------All Dead Ends Processed---------------");
+        // Print location and key #
+        for (Map.Entry<Coordinate, Integer> key : keys.entrySet()) {
+        		System.out.println("The key " + key.getValue() + " is at " + key.getKey());
+        }
+        System.out.println("---------------All Keys Processed---------------");
+        
+        // Print location of dead ends
+        for (Map.Entry<Coordinate, String> point : pointsOfInterest.entrySet()) {
+        		System.out.println("The coordinate " + point.getKey() + " is the tile of " + point.getValue() + " !");
+        }
+        System.out.println("---------------All Dead Ends Processed---------------");
 	}
 }
